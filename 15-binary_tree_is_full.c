5135a0e3017d874e6b00ffa6f59a1b307f0ae1cd
cmeynet <clemence.meynet14@gmail.com>
2025-04-08 17:24:57 +0200

    fix 15-binary_tree_is_full change condition if leaves

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,14 +9,20 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left_full = 0, right_full = 0;
+
 	if (!tree)
 		return (0);
 
-	if (tree->parent && tree->left && tree->right)
-	{
-		binary_tree_is_full(tree->left);
-		binary_tree_is_full(tree->right);
+	if (!tree->left && !tree->right)
 		return (1);
+
+	if (tree->left && tree->right)
+	{
+		left_full = binary_tree_is_full(tree->left);
+		right_full = binary_tree_is_full(tree->right);
+		if (left_full == 1 && right_full == 1)
+			return (1);
 	}
 	return (0);
 }
