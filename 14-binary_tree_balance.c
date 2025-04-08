223fba50b7cb960bc2238ff19e457148b5f89233
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-08 14:05:24 +0200

    adding file 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,9 +3,13 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t left = binary_tree_height(tree->left);
+	size_t right = binary_tree_height(tree->right);
+
 	if (!tree)
 	{
 		return (0);
 	}
-	
+
+	return (left - right);
 }
